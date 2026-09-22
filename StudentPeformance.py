cbf042481d7920a5bc191ce98b8f5d6ba9c21961
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-09-22 10:19:40 +0700

    add second student perform

--- StudentPeformance.py
+++ StudentPeformance.py
@@ -3,3 +3,5 @@ peformance = input("Please Input The Student Performance: ")
 
 if int (performance) >= 90:
     print ("Excellent Performance") 
+elif peformance >=80:
+    print ("Very Good performance")
