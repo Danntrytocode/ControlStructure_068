4a623e6c8531dcb5653e133e68fed992e846f87d
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-09-22 10:37:05 +0700

    fixing wrong logic

--- StudentPeformance.py
+++ StudentPeformance.py
@@ -1,12 +1,13 @@
-performance = input("Please Input The Student Performance: ")
+performance = int(input("Please Input The Student Performance: "))
     #peformance == %:
 
-if int (performance) >= 90:
+if performance >= 90:
     print ("Excellent Performance") 
-elif (performance) >=80:
+elif performance >= 80:
     print ("Very Good performance")
-elif (performance) >=70:
+elif performance >= 70:
     print ("Good Performance")
-else:
+elif performance >= 60:
     print ("Average Performance")
-print ("Keep Learning!")
+else :
+    print ("Keep Learning!") 
