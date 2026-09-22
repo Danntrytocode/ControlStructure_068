1c6cf41a4582d8785bf7d95dfbf134e0f7993e07
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-09-22 11:06:23 +0700

    add condition and option

--- TheLargestofThree.py
+++ TheLargestofThree.py
@@ -1,3 +1,15 @@
 x = int(input("Please Type First Number: "))
 y = int(input("Please Type Secong Number: "))
 z = int(input("Please Type Third Number: "))
+
+if x > y and x > z:
+    largest = x
+    print("The largest is: ", x)
+elif y > x and y > z:
+    largest = y
+    print("The largest is: ". y)
+elif z > x and z > y:
+    largest = z
+    print("The largest is: ", z)
+else:
+    ("There no Largest Number")
